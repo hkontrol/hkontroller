9919212db3827a4c76be208c8826a352a8c4224e
Vladimir Shabunin <v.shabunin@aladdin.ru>
2022-10-12 15:55:07 +0300

    add pair

---
+++ controller-pair-add.go
@@ -0,0 +1,65 @@
+package hkontroller
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/hkontrol/hkontroller/tlv8"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+type pairAddReqPayload struct {
+	State       byte   `tlv8:"6"`
+	Method      byte   `tlv8:"0"`
+	Identifier  string `tlv8:"1"`
+	PublicKey   []byte `tlv8:"3"`
+	Permissions byte   `tlv8:"11"`
+}
+
+type pairAddResPayload struct {
+	State byte `tlv8:"6"`
+	Error byte `tlv8:"7"`
+}
+
+// PairAdd serves to pair another controller.
+func (c *Controller) PairAdd(d *Device, p Pairing) error {
+	pl := pairAddReqPayload{
+		State:       M1,
+		Method:      MethodAddPairing,
+		Identifier:  p.Name,
+		PublicKey:   p.PublicKey,
+		Permissions: p.Permission,
+	}
+	b, err := tlv8.Marshal(pl)
+	if err != nil {
+		return err
+	}
+
+	resp, err := d.httpc.Post("/pairings", HTTPContentTypePairingTLV8, bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code %v", resp.StatusCode)
+	}
+	res := resp.Body
+	defer res.Close()
+	all, err := ioutil.ReadAll(res)
+	if err != nil {
+		return err
+	}
+	m2 := pairAddResPayload{}
+	err = tlv8.Unmarshal(all, &m2)
+
+	if m2.Error != 0x00 {
+		return errors.New("res.err = " + strconv.FormatInt(int64(m2.Error), 10))
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
